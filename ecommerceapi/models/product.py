52b5d33b9c8fa8a4a014579cacf037193837022c
Roxanne Nasraty <rsnasraty@gmail.com>
2020-08-25 15:40:43 -0500

    Merge conflicts

--- ecommerceapi/models/product.py
+++ ecommerceapi/models/product.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from .
+from 
 from .product_type import ProductType
 
 class Product(models.Model):
