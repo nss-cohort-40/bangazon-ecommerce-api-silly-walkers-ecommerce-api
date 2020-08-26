5d98f1e2a49b600dda4df0c460e9d0af7a11af79
Roxanne Nasraty <rsnasraty@gmail.com>
2020-08-26 14:05:41 -0500

    Updated views/models

--- ecommerceapi/views/product.py
+++ ecommerceapi/views/product.py
@@ -16,7 +16,7 @@ class ProductSerializer(serializers.HyperlinkedModelSerializer):
             view_name='product',
             lookup_field='id'
         )
-        fields = ('id', 'url', 'title', 'account_number', 'quantity', 'description' 'created_at', 'customer', 'product_type_Id', 'imagePath')
+        fields = ('id', 'url', 'title', 'quantity', 'price', 'description', 'customer', 'imagePath', 'location', 'producttype')
         depth= 1
 
 
@@ -24,15 +24,18 @@ class Products(ViewSet):
 
     def create(self, request):
         new_product = Product()
+        product_type = Product.objects.get(pk=request.data["producttype_id"])
+        customer = Customer.objects.get(user=request.auth.user)
+        
         new_product.title = request.data["title"]
         new_product.description = request.data["description"]
         new_product.quantity = request.data["quantity"]
-        new_product.account_number = request.data["account_number"]
-        new_product.customer = request.data["customer"]
-        location = Product.objects.get(pk=request.data["product_id"])
-        imagePath = Product.objects.get(pk=request.data["imagePath"])
-        new_product.created_at = request.data["created_at"]
-        product.location = location
+        new_product.location = request.data["location"]
+        new_product.imagePath = request.data["imagePath"]
+        new_product.price = request.data["price"]
+        new_product.customer = customer
+        new_product.product_type = product_type
+        
         new_product.save()
 
         serializer = ProductSerializer(new_product, context={'request': request})
@@ -47,20 +50,10 @@ class Products(ViewSet):
         except Exception as ex:
             return HttpResponseServerError(ex)
 
-    def update(self, request, pk=None):
-        product = Product.objects.get(pk=pk)
-        location = ProductLocation.objects.get(pk=request.data["location_id"])
-        product.title = request.data["title"]
-        product.created_at = request.data["created_at"]
-        product.location = location
-        product.save()
-
-        return Response({}, status=status.HTTP_204_NO_CONTENT)
 
     def destroy(self, request, pk=None):
         try:
             product = Product.objects.get(pk=pk)
-            location.delete()
             product.delete()
 
             return Response({}, status=status.HTTP_204_NO_CONTENT)
@@ -73,9 +66,6 @@ class Products(ViewSet):
 
     def list(self, request):
         products = Product.objects.all()
-        location = self.request.query_params.get('location', None)
-        if location is not None:
-            products = products.filter(location__id=location)
         serializer = ProductSerializer(
             products, many=True, context={'request': request})
         return Response(serializer.data)
