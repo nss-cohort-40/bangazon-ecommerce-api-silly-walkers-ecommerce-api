f715afdcbea1a685e9236483a2c11e299220e72b
Tom Strother <tom_strother@yahoo.com>
2020-08-27 15:23:43 -0500

    sql

--- ecommerceapi/models/product.py
+++ ecommerceapi/models/product.py
@@ -1,8 +1,15 @@
 from django.db import models
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from .customer import Customer
 from .product_type import ProductType
+from .customer import Customer
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+INSERT INTO ecommerceapi_orderproduct(id, order_id, product_id)
+VALUES(
+    id: integer,
+    order_id: integer,
+    product_id: integer
+)
+
 
 class Product(models.Model):
 
